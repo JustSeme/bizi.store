44d4abc1121b26db937414b76b7d11f71a2b2623
JustSemen <semyn03@mail.ru>
2023-02-23 12:47:24 +0300

    add form sender to google sheets

--- js/script.js
+++ js/script.js
@@ -115,24 +115,53 @@ $(document).ready(function () {
         }
     })
 
-    $('.js-form-btn-2').click(function () {
-        const buttonDataModal = $(this).attr('data-modal')
-        const data = {
-            name: $(`input[id=name][data-modal=${buttonDataModal}]`).val(),
-            city: $(`input[id=city][data-modal=${buttonDataModal}]`).val(),
-            contact: $(`input[id=contact][data-modal=${buttonDataModal}]`).val(),
-            productTitle: productTitle ? productTitle : 'none'
-        }
-        console.log(data);
+    $('.js-form-btn-2').click(function (event) {
+        event.preventDefault();
+
+        // Ссылка, которую получили на этапе публикации приложения
+        let appLink = "https://script.google.com/macros/s/AKfycbyXPI49IudPZB1g-sX8UwyzxlEgdYLKLitJEnNAC4oDjRQE3nKwx69hBwlgEpsKD7Dccg/exec";
+
+        // Id текущей формы
+        let form = $('#' + $(this).attr('data-form'))[0];
+
+        // Кнопка отправки формы
+        let submitButton = $(this)
+
+        // FormData
+        let fd = new FormData(form);
 
         $.ajax({
-            method: "GET",
-            url: "https://first-api-sigma.vercel.app/courses",
-            contentType: 'application/json'
-            /* data: { name: "John", location: "Boston" } */
-        })
-            .done(function (msg) {
-                alert("Data Saved: " + msg);
-            });
-    })
-});
+
+            url: appLink,
+            type: "POST",
+            data: fd,
+            processData: false,
+            contentType: false,
+            beforeSend: function () {
+
+                // Делаем неактивной кнопку отправки
+                submitButton.prop('disabled', true);
+            },
+
+        }).done(function (res, textStatus, jqXHR) {
+
+            if (jqXHR.readyState === 4 && jqXHR.status === 200) {
+
+                // Возвращаем активность кнопке отправки
+                submitButton.prop('disabled', false);
+
+                // Очищаем поля формы
+                form.reset();
+
+            } else {
+                console.log('Гугл не ответил статусом 200');
+            }
+        }).fail(function (res, textStatus, jqXHR) {
+            setTimeout(() => {
+                submitButton.prop('disabled', false);
+            }, 5000);
+
+            console.log('Не удалось выполнить запрос по указанному в скрипте пути');
+        });
+    });
+}(jQuery));
