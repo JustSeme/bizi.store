a3da4b3e70a54032fe2617d59a3baffb5654a246
JustSemen <semyn03@mail.ru>
2023-03-05 23:14:32 +0300

    autoplay

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         items: 4,
         dotsEach: 3,
         loop: true,
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 10000,
         margin: 50,
         navText: [
